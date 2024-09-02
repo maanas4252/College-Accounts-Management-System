65ad3295d285767b20da22cbe2a648916f45500e
maanas4252 <138567626+maanas4252@users.noreply.github.com>
2024-09-02 12:29:26 +0530

    Add files via upload

---
+++ College.java
@@ -0,0 +1,50 @@
+package college.accounts.management.system;
+
+import java.util.List;
+
+public class College {
+
+    private List<Teacher> teachers;
+    private List<Student> students;
+    private static int totalMoneyEarned;
+    private static int totalMoneySpent;
+
+    public College(List<Teacher> teachers, List<Student> students) {
+        this.teachers = teachers;
+        this.students = students;
+        totalMoneyEarned = 0;
+        totalMoneySpent = 0;
+    }
+
+    public List<Teacher> getTeachers() {
+        return teachers;
+    }
+
+    public void addTeacher(Teacher teacher) {
+        teachers.add(teacher);
+    }
+
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        students.add(student);
+    }
+
+    public int getTotalMoneyEarned() {
+        return totalMoneyEarned;
+    }
+
+    public static void updateTotalMoneyEarned(int moneyEarned) {
+        totalMoneyEarned += moneyEarned;
+    }
+
+    public int getTotalMoneySpent() {
+        return totalMoneySpent;
+    }
+
+    public static void updateTotalMoneySpent(int moneySpent) {
+        totalMoneyEarned -= moneySpent;
+    }
+}
